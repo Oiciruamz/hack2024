2c5f0b296bc57370be1742b31e18fa1e7a336b84
marlonsalc <marloneduardosalc@gmail.com>
2024-05-26 02:39:24 -0600

    Restructura de archivos

--- AI_prueba2.py
+++ AI_prueba2.py
@@ -3,24 +3,32 @@ from PIL import Image
 import streamlit as st
 import main
 
+from pdfDown import PDF, create_pdf
+
+st.set_page_config(page_title="Traductor de texto a Braille", page_icon="🔠", layout="wide")
+
 # Implementación del header con HTML y CSS
 st.markdown("""
     <style>
+    @import url('https://fonts.googleapis.com/css2?family=Inria+Serif:wght@400;700&display=swap');
+
     .header {
-        background-color: #0d47a1;
+        background-color: #18407B;
         padding: 20px;
         display: flex;
         justify-content: space-between;
+        box-shadow: 0 4px 6px 0 black;
         align-items: center;
-        color: white;
         width: 100%;
         box-sizing: border-box;
+        font-family: 'Inria Serif', serif;
     }
     .header-left {
         text-align: left;
     }
     .header-left h1, .header-left .braille {
         margin: 0;
+        font-family: 'Inria Serif', serif;
     }
     .header-left h1 {
         font-size: 2.5em;
@@ -32,12 +40,23 @@ st.markdown("""
     .header-right {
         text-align: right;
         font-size: 1.5em;
+        font-family: 'Inria Serif', serif;
+    }
+    body, h1, h2, h3, h4, h5, h6, p, div {
+        font-family: 'Inria Sans', sans;
+    }
+    .braille-container {
+        background-color: #231c1c;
+        padding: 20px;
+        border-radius: 10px;
+        box-shadow: 0 4px 6px 0 rgba(0, 0, 0, 0.1);
+        margin-top: 20px;
     }
     </style>
     <div class="header">
         <div class="header-left">
             <h1>Letras inclusivas</h1>
-            <div class="braille">⠠⠇⠑⠞⠁⠎ ⠊⠝⠉⠇⠥⠎⠊⠧⠁⠎</div>
+            <div class="braille">⠠⠇⠑⠗⠁⠎ ⠊⠝⠉⠇⠥⠎⠊⠧⠁⠎</div>
         </div>
         <div class="header-right">
             <h2>Cambiando el mundo un texto a la vez</h2>
@@ -77,13 +96,21 @@ if image_path is not None:
         lines = count_text_lines(image_path)
         for line in lines:
             texto += line + " "
-            
-        st.subheader("Texto extraído de la imagen:")   
-        st.write(texto)
         
         braille = main.user_text(texto)
         st.subheader("Texto traducido a Braille:")
-        st.markdown(braille)
+        st.markdown(f"""
+            <div class="braille-container">
+                {braille}
+            </div>
+        """, unsafe_allow_html=True)
+        def create_pdf(text, file_path):
+            pdf = PDF()
+            pdf.download_braille(text, file_path)
+        
+        
+        st.button("Descargar Braille", on_click=create_pdf(braille, "mi_documento_braille.pdf"))
+            
     
     except Exception as e:
         st.write("An error occurred: {}".format(e))
--- hola.py
+++
@@ -1,7 +0,0 @@
-import main
-
-user_text = "Letras Inclusivas"
-
-braile_text = main.user_text(user_text)
-
-print(braile_text)
